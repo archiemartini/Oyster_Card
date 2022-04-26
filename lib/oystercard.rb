90ea996189047e2a18b43dc50e7e3c9f785691c9
sjtinsley <simonjtinsley@gmail.com>
2022-04-26 15:32:22 +0100

    Added ability for user to be charged for a journey

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,29 +1,30 @@
 class OysterCard
-  attr_reader :balance
+  attr_reader :balance, :in_use
   MINIMUM_BALANCE = 1
   MAXIMUM_BALANCE = 90
+  INITIAL_BALANCE = 0
+
   def initialize
-    @balance = 0
+    @balance = INITIAL_BALANCE
     @in_use = false
-  end 
+  end
+
   def top_up(money)
     raise "Cannot exceed £#{MAXIMUM_BALANCE} balance" if @balance + money > MAXIMUM_BALANCE
     @balance += money
   end
-  def deduct(money)
-    @balance -= money
-  end
-  
-  def in_journey?
-    @in_use
-  end 
-  
+
   def touch_in
-    @balance < 1 ? raise("Insufficient funds, balance must be at least £#{MINIMUM_BALANCE}") : @in_use = true
+    @balance < MINIMUM_BALANCE ? raise("Insufficient funds, balance must be at least £#{MINIMUM_BALANCE}") : @in_use = true
   end
-  
+
   def touch_out
+    deduct(MINIMUM_BALANCE)
     @in_use = false
   end
-  
+
+  private
+    def deduct(money)
+      @balance -= money
+    end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,17 +13,12 @@ describe OysterCard do
 
   it 'cannot be topped up above maximum balance' do
     expect { subject.top_up(OysterCard::MAXIMUM_BALANCE + 1) }.to raise_error 'Cannot exceed £90 balance'
-  end 
-
-  it 'can deduct money' do
-    subject.top_up(32)
-    expect(subject.deduct(2)).to eq 30
   end
 
   it 'can touch in' do
     expect(subject).to respond_to(:touch_in)
   end
-  
+
   it 'can touch out' do
     expect(subject).to respond_to(:touch_out)
   end
@@ -31,10 +26,16 @@ describe OysterCard do
   it 'knows when it is on a journey' do
     subject.top_up(OysterCard::MINIMUM_BALANCE)
     subject.touch_in
-    expect(subject.in_journey?).to eq true
+    expect(subject.in_use).to eq true
   end
 
   it 'won\'t let user travel without minimum balance' do
     expect { subject.touch_in }.to raise_error "Insufficient funds, balance must be at least £#{OysterCard::MINIMUM_BALANCE}"
-  end 
+  end
+
+  it 'deducts balance on touch out' do
+    subject.top_up(OysterCard::MINIMUM_BALANCE)
+    allow(subject).to receive(:in_use) {true}
+    expect{ subject.touch_out }.to change{ subject.balance }.by(-OysterCard::MINIMUM_BALANCE)
+  end
 end
